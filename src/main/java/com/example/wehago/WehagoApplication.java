ea95af3fdee5bf6633ef604f545191dd62eb76dd
eunhye17 <eunhye17@chunjae.co.kr>
2025-04-22 22:07:31 +0900

    프로젝트 초기 세팅 및 Readme 생성

---
+++ src/main/java/com/example/wehago/WehagoApplication.java
@@ -0,0 +1,13 @@
+package com.example.wehago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WehagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WehagoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wehago/WehagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wehago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WehagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
