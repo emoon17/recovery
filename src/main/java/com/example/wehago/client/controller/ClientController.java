2d6e68b0f5fa0a3bf852116c00ac15af35f72859
eunhye17 <eunhye17@chunjae.co.kr>
2025-04-28 10:48:50 +0900

    [fix] db 연결 -> 도커 db 로 변경

--- src/main/java/com/example/wehago/client/controller/ClientController.java
+++ src/main/java/com/example/wehago/client/controller/ClientController.java
@@ -2,23 +2,25 @@ package com.example.wehago.client.controller;
 
 import com.example.wehago.client.dto.ClientEntity;
 import com.example.wehago.client.mapper.ClientMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
-public class Controller {
+public class ClientController {
 
     private ClientMapper clientMapper;
 
-    public Controller(ClientMapper clientMapper) {
+    public ClientController(ClientMapper clientMapper) {
         this.clientMapper = clientMapper;
     }
 
     @GetMapping("/test/id")
     public String getClientEntity() {
         ClientEntity clientEntity = clientMapper.findById(1);
-        return clientEntity.getName();
+        log.info(clientEntity.getName());
+        return clientEntity.getEmail();
     }
 
 }
