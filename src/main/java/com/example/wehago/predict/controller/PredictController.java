47fd6d8d3260f954d9d148d30ced56e42a3af843
eunhye17 <wjddmsgo45@naver.com>
2025-05-04 20:03:06 +0900

    [add] 파이썬 api 통신 테스트

---
+++ src/main/java/com/example/wehago/predict/controller/PredictController.java
@@ -0,0 +1,32 @@
+package com.example.wehago.predict.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/predict")
+public class PredictController {
+
+    private final WebClient webClient;
+
+    public PredictController(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    @PostMapping("/delayDays")
+    public Mono<String> predictDelay (@RequestBody Map<String, String> request) {
+        return webClient.post()
+                .uri("/predict")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(request)
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+}
